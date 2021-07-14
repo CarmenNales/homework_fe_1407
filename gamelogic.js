51bc786d90a4a0124c32cfde26ddbef51101e577
Carmen <56934545+CarmenNales@users.noreply.github.com>
2021-07-14 23:40:41 +0200

    Opdracht zonder comments klaar

--- gamelogic.js
+++ gamelogic.js
@@ -1,17 +1,54 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  let displayedWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    let guessedLetter = false;
+
+    for (let j = 0; j < guesses.length; j++) {
+      if (guesses[j] === word[i]) {
+        displayedWord += guesses[j];
+        guessedLetter = true;
+        break;
+      }
+    }
+
+    if (!guessedLetter) {
+      displayedWord += "_";
+    }
+
+    displayedWord += " ";
+  }
+
+  return displayedWord;
+}
+
+function getWrongGuesses(word, guesses) {
+  let wrongLetters= [];
+
+  for (let i = 0; i < guesses.length; i++) {
+    if (!word.includes(guesses[i])) {
+      wrongLetters.push(guesses[i]);
+    }
+  }
+
+  return wrongLetters;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  let displayWord = displayWordSoFar(word, guesses);
+  displayWord = displayWord.split(" ").join("");
+  return word === displayWord;
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  let wrongGuesses = getWrongGuesses(word, guesses);
+  const maxWrongNumbers = 7;
+  return wrongGuesses.length >= maxWrongNumbers;
 }
 
 module.exports = {
   displayWordSoFar: displayWordSoFar,
+  getWrongGuesses: getWrongGuesses,
   isGameWon: isGameWon,
   isGameLost: isGameLost,
 };
--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
+const { displayWordSoFar, getWrongGuesses, isGameWon, isGameLost } = require("./gamelogic");
 
 function game(word, guesses) {
-  console.log("Dit heb je tot nu toe geraden: ", guesses);
 
-  const letter = question("Raad een letter: ");
+  const letter = question("Raad een letter: ").toLowerCase();
+
+
+  if (letter.length !== 1) {
+    console.error("Foutmelding: Voer hier 1 letter in");
+    return game(word, guesses);
+  }
+
+  if (guesses.includes(letter)) {
+    console.error("Foutmelding: Deze letter heb je al gebruikt");
+    return game(word, guesses);
+  }
 
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
+
+  const displayWord = displayWordSoFar(word, guesses)
+  const wrongGuesses = getWrongGuesses(word, guesses)
+  const tellWrongNumbers = wrongGuesses.length
+  const isLost = isGameLost(word, wrongGuesses)
+  const isWon = isGameWon(word, displayWord)
+
+  console.log(`Jouw woord is: ${displayWord}`)
+  console.log(`Deze letters zitten niet in het woord: ${wrongGuesses}, aantal fouten: ${ tellWrongNumbers }.`)
+
+  if (isLost) {
+    console.log("Je hebt teveel fouten gemaakt, je hebt verloren");
+    return;
+  }
+
+  if (isWon) {
+    console.log("Gefeliciteerd, je hebt gewonnen!");
+    return;
+  }
 
   // volgende ronde! we roepen game nog een keer aan
   game(word, guesses);
